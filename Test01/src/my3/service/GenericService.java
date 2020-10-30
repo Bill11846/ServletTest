f987962d7fd7ae3504b236ceb4ca8935cb762221
bill <bill11846@163.com>
2020-10-30 23:31:06 +0800

    test add commit

---
+++ Test01/src/my3/service/GenericService.java
@@ -0,0 +1,11 @@
+package my3.service;
+
+/**
+ * @BelongsProject: WebApplicationSecondary
+ * @BelongsPackage: my3.service
+ * @Author: billzhang
+ * @CreateTime: 2020-10-30
+ * @Description:
+ */
+public class GenericService {
+}
