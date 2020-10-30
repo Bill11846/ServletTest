0e03aa6b02730f0b2a666d47314a1899621f2d83
bill <bill11846@163.com>
2020-10-30 22:22:29 +0800

    initial commit

---
+++ Test01/src/my1/TimeNowService.java
@@ -0,0 +1,32 @@
+package my1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @BelongsProject: WebApplicationSecondary
+ * @BelongsPackage: ${PACKAGE_NAME}
+ * @Author: billzhang
+ * @CreateTime: 2020-10-30
+ * @Description: ${Description}
+ */
+@WebServlet(name = "TimeNowService",urlPatterns = "/TimeNow")
+public class TimeNowService extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String text = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now());
+
+        response.setContentType("text/plain;charset=utf-8");
+        response.setHeader("Connection","Close");
+        PrintWriter writer = response.getWriter();
+        writer.write(text);
+        writer.close();
+    }
+}
---
+++ Test01/web/index.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: billzhang
+  Date: 2020/10/30
+  Time: 10:15 PM
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
