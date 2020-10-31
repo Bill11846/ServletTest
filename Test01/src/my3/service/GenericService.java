d289b964ba7e2ad838e45ba59e378942cb7694c1
bill <bill11846@163.com>
2020-10-31 10:26:48 +0800

    add service.GenericService to github test

--- Test01/src/my3/service/GenericService.java
+++ Test01/src/my3/service/GenericService.java
@@ -1,6 +1,10 @@
 package my3.service;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.nio.charset.Charset;
 
 /**
@@ -15,6 +19,30 @@ public class GenericService extends HttpServlet {
     protected String charset = Charset.forName("UTF-8").toString();
     protected boolean enableErrorLog = false;
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            handleRequest(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendError(500, e.getMessage());
+        }
+    }
+
+
+    private void handleRequest(HttpServletRequest request,HttpServletResponse response)throws Exception{
+        String webRootPath = request.getServletPath();
+        int p1 = webRootPath.lastIndexOf('/');
+        int p2 = webRootPath.lastIndexOf('.');
+        String apiName = webRootPath.substring(p1+1,p2);
+        System.out.println("Obtain Service Name:" + apiName);
+
+
+    }
 
 }
